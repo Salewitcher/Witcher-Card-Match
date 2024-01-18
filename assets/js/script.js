ca027d4d5991ca4b89c889d01c2d754c6551f86d
Salewitcher <>
2024-01-18 19:28:13 +0000

    Game did not work so code taken from youtube ,url in comments. images do not show must be src path error

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,9 +25,8 @@ window.onclick = function (event) {
     }
 };
 
-// Track mistakes
-var mistakes = 0;
-// Card Names
+// Code from "https://www.youtube.com/watch?v=wz9jeI9M9hI"
+var errors = 0;
 var cardList = [
     "ciri",
     "dethlaff",
@@ -41,55 +40,102 @@ var cardList = [
     "yennefer"
 ];
 
+
 var cardSet;
-var gameBoard = [];
+var board = [];
 var rows = 4;
 var columns = 5;
 
-// Page Load
+var card1Selected;
+var card2Selected;
+
 window.onload = function () {
     shuffleCards();
     startGame();
 };
 
 function shuffleCards() {
-    cardSet = cardList.concat(cardList); //Duplicate cards
-}
-// Shuffle cards
-for (let i = 0; i < cardSet.lenght; i++) {
-    let r = Math.floor(Math.random() * cardSet.lenght);
-    let temporary = cardSet[i];
-    cardSet[i] = cardSet[t];
-    cardSet[t] = temporary;
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //get random index
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
 }
 
 function startGame() {
-    // Set cards on board
+    //arrange the board 4x5
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
             let cardImg = cardSet.pop();
-            row.push(cardImg);
+            row.push(cardImg); //JS
 
-            // <img>
-            let cards = document.createElement('img');
-            cards.id = r.toString() + '-' + c.toString();
-            cards.src = cardImg + '.jpg';
-            cards.classList.add('cards');
-            document.getElementById('game-board').append(cards);
+            // <img id="0-0" class="card" src="water.jpg">
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = cardImg + ".jpg";
+            card.classList.add("card");
+            card.addEventListener("click", selectCard);
+            document.getElementById("board").append(card);
+
         }
-        game - board.push(row);
+        board.push(row);
     }
 
-    console.log(game - board);
+    console.log(board);
     setTimeout(hideCards, 1000);
 }
 
 function hideCards() {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
-            let cards = document.getElementById(r.toString() + "-" + c.toString());
-            cards.src = "back-side.jpg";
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "back.jpg";
         }
     }
+}
+
+function selectCard() {
+
+    if (this.src.includes("back")) {
+        if (!card1Selected) {
+            card1Selected = this;
+
+            let coords = card1Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = board[r][c] + ".jpg";
+        }
+        else if (!card2Selected && this != card1Selected) {
+            card2Selected = this;
+
+            let coords = card2Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = board[r][c] + ".jpg";
+            setTimeout(update, 1000);
+        }
+    }
+
+}
+
+function update() {
+    //if cards aren't the same, flip both back
+    if (card1Selected.src != card2Selected.src) {
+        card1Selected.src = "back.jpg";
+        card2Selected.src = "back.jpg";
+        errors += 1;
+        document.getElementById("errors").innerText = errors;
+    }
+
+    card1Selected = null;
+    card2Selected = null;
 }
